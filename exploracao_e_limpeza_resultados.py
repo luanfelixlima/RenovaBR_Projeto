abf4a45efe9dbb9375e9797e5c5d619d1bd69e8f
luanfelixlima <luan.felix.lima@outlook.com>
2023-08-16 10:20:07 -0300

    Selecionando os dados que iremos usar da base de dados do perfil eleitorado

---
+++ exploracao_e_limpeza_resultados.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Carregando os dados em um DataFrame através da biblioteca "Pandas"
+df = pd.read_csv('database_desfio_renovaBR\\resultados\\SP_turno_1.csv',
+                 encoding='latin-1', sep=';')
