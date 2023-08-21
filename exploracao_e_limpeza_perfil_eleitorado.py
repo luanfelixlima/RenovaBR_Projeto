67e7f7c71f7f4130f538d69121f7628b77a91591
luanfelixlima <luan.felix.lima@outlook.com>
2023-08-20 23:27:14 -0300

    Melhorar a limpeza (dropando mais colunas desnecessárias para as consultas)

--- exploracao_e_limpeza_perfil_eleitorado.py
+++ exploracao_e_limpeza_perfil_eleitorado.py
@@ -8,7 +8,7 @@ df = pd.read_csv('database_desfio_renovaBR\\'
 
 
 # SELECIONANDO O ESTADO DE SP:
-print(df['SG_UF'].value_counts(), "\n")  # agrupando e contando os valores de cada dado do DataFrame
+print(df['SG_UF'].value_counts(), "\n")  # value_counts() - agrupando e contando os valores de cada dado do DataFrame
 # Através da Consulta vemos que SP tem 639502 registros
 dados_SP = df[df['SG_UF'] == 'SP']  # Filtrando o municipio por condição booleana
 print(dados_SP['SG_UF'].value_counts())  # Verificando se há de fato 639502 registros
@@ -18,7 +18,9 @@ print(dados_SP['SG_UF'].value_counts())  # Verificando se há de fato 639502 reg
 print(dados_SP.head(5), "\n")
 dados_relevantes_SP = dados_SP.drop(
     ['DT_GERACAO', 'HH_GERACAO', 'ANO_ELEICAO', 'CD_MUN_SIT_BIOMETRIA', 'DS_MUN_SIT_BIOMETRIA',
-     'QT_ELEITORES_INC_NM_SOCIAL', 'QT_ELEITORES_BIOMETRIA', 'QT_ELEITORES_PERFIL'],
+     'QT_ELEITORES_INC_NM_SOCIAL', 'QT_ELEITORES_BIOMETRIA', 'QT_ELEITORES_PERFIL',
+     'SG_UF', 'CD_MUNICIPIO', 'NR_ZONA', 'CD_GENERO', 'CD_ESTADO_CIVIL',
+     'CD_GRAU_ESCOLARIDADE', 'QT_ELEITORES_DEFICIENCIA'],
     axis=1
 )  # drop - apaga as colunas que passamos como argumentos.
 print(dados_relevantes_SP.head(5), "\n")  # head - mostra as primeiras linhas do DataFrame
@@ -29,10 +31,10 @@ print(dados_relevantes_SP.head(5), "\n")  # head - mostra as primeiras linhas do
 print(dados_relevantes_SP['CD_FAIXA_ETARIA'].value_counts(), "\n")  # Podemos ver 268 dados inválidos
 dados_relevantes_SP = dados_relevantes_SP[dados_relevantes_SP['CD_FAIXA_ETARIA'] != -3]  # -3 é inválido segundo o dicionário de dados
 print(dados_relevantes_SP['CD_FAIXA_ETARIA'].value_counts())  # verificando se realmente apagamos registros de idades inválidas
-print(dados_relevantes_SP.shape, "\n")  # temos que ter 639502 - 268 linhas
-
+print(dados_relevantes_SP.shape, "\n")  # shape - retorna as dimensões do dataframe | temos que ter 639502 - 268 linhas
+dados_relevantes_SP = dados_relevantes_SP.drop(['CD_FAIXA_ETARIA'], axis=1)
 
 # EXPORTANDO OS DADOS LIMPOS:
 dados_relevantes_SP.to_csv('database_desfio_renovaBR\\dados_filtrados\\perfil_eleitorado.csv', sep=';',
-                           encoding='latin-1')
+                           encoding='latin-1')  # salvando o dataframe em um arquivo csv
 print("Exportando dados...")
--- exploracao_e_limpeza_resultados.py
+++ exploracao_e_limpeza_resultados.py
@@ -17,17 +17,20 @@ dados_relevantes = df.drop(
      'NR_URNA_EFETIVADA', 'CD_CARGA_1_URNA_EFETIVADA', 'CD_CARGA_2_URNA_EFETIVADA', 'CD_FLASHCARD_URNA_EFETIVADA',
      'DT_CARGA_URNA_EFETIVADA', 'DS_CARGO_PERGUNTA_SECAO', 'DS_AGREGADAS', 'DT_ABERTURA', 'DT_ENCERRAMENTO',
      'QT_ELEITORES_BIOMETRIA_NH', 'DT_EMISSAO_BU', 'NR_JUNTA_APURADORA', 'NR_TURMA_APURADORA', 'CD_MUNICIPIO',
-     'QT_APTOS', 'QT_COMPARECIMENTO', 'QT_ABSTENCOES', 'CD_TIPO_VOTAVEL', 'DS_TIPO_VOTAVEL'],
+     'QT_APTOS', 'QT_COMPARECIMENTO', 'QT_ABSTENCOES', 'CD_TIPO_VOTAVEL', 'DS_TIPO_VOTAVEL',
+     'NR_ZONA', 'NR_SECAO', 'NR_LOCAL_VOTACAO', 'CD_CARGO_PERGUNTA'],
     axis=1
-)
+)  # drop - apaga as colunas que passamos como argumentos.
 
 # REMOVENDO DADOS INVÁLIDOS:
-print(dados_relevantes['NR_PARTIDO'].value_counts(),
+print(dados_relevantes['NR_PARTIDO'].value_counts(),  # value_counts() - agrupando e contando os valores de cada dado do DataFrame
       "\n")  # valores com -1 são inválidos, faz que nem o nome do candidato seja válido
 print(dados_relevantes.shape[0])  # Conferindo a quantidade de linhas
 dados_relevantes = dados_relevantes[dados_relevantes['NR_PARTIDO'] != -1]  # removendo valores iguais a -1
 print(dados_relevantes['NR_PARTIDO'].value_counts(), "\n")
-print(dados_relevantes.shape[0])
+print(dados_relevantes.shape[0])  # shape - retorna as dimensões do dataframe
+
+dados_relevantes = dados_relevantes.drop(['NR_PARTIDO'], axis=1)
 
 print(dados_relevantes['NM_VOTAVEL'].value_counts(), "\n")
 dados_relevantes = dados_relevantes[dados_relevantes['SG_PARTIDO'] != dados_relevantes[
@@ -37,5 +40,5 @@ print(dados_relevantes['NM_VOTAVEL'].value_counts(), "\n")
 
 # EXPORTANDO OS DADOS LIMPOS:
 dados_relevantes.to_csv('database_desfio_renovaBR\\dados_filtrados\\resultados.csv', sep=';',
-                        encoding='latin-1')
+                        encoding='latin-1')  # salvando o dataframe em um arquivo csv
 print("Exportando dados...")
