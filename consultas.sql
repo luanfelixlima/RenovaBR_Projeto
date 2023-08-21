07a28bf9550a8526a275a500c8ef968cbacc4e46
luanfelixlima <luan.felix.lima@outlook.com>
2023-08-20 22:54:29 -0300

    finalizando as consultas

--- consultas.sql
+++ consultas.sql
@@ -1,13 +1,21 @@
--- Quantidade de votos vencedores de cada município
-WITH SOMA_VOTOS AS 
+-- 1) Qual município o candidato X foi mais votado;
+SELECT NM_MUNICIPIO, NM_VOTAVEL FROM resultados
+where NM_VOTAVEL = 'Auricchio'
+GROUP BY NM_VOTAVEL, NM_MUNICIPIO;
+
+
+-- 2) Qual candidato foi mais votado em cada município;
+WITH TABELAS AS 
 (
-	SELECT NM_VOTAVEL, SUM(QT_VOTOS) AS QT_VOTOS FROM resultados
-	GROUP BY NM_VOTAVEL
+SELECT DISTINCT NM_MUNICIPIO, SUM(QT_VOTOS) as QT_VOTOS, NM_VOTAVEL FROM RESULTADOS
+GROUP BY NM_VOTAVEL, NM_MUNICIPIO  -- Exportado como BD
 )
-SELECT r.NM_MUNICIPIO, MAX(s.QT_VOTOS) AS MAX_VOTOS
-FROM resultados AS r inner join SOMA_VOTOS AS s
-on r.QT_VOTOS = s.QT_VOTOS
-GROUP BY NM_MUNICIPIO
-ORDER BY MAX_VOTOS DESC;
+SELECT DISTINCT t.NM_MUNICIPIO, NM_VOTAVEL, v.QT_VOTOS as VOTOS FROM votos v
+left join TABELAS t on t.QT_VOTOS = v.QT_VOTOS and t.NM_MUNICIPIO = v.NM_MUNICIPIO
+order by v.QT_VOTOS desc;
 
-
+-- 3) Qual perfil do eleitorado (faixa etária, gênero, grau de escolaridade, etc.) mais votou em cada candidato.select top 1 DS_FAIXA_ETARIA, DS_GENERO, DS_GRAU_ESCOLARIDADE, DS_ESTADO_CIVIL, count(*) as QUANTIDADE
+from perfil_eleitorado
+where NM_MUNICIPIO = 'São Paulo' -- Especificar a cidade
+group by DS_FAIXA_ETARIA, DS_GENERO, DS_GRAU_ESCOLARIDADE, DS_ESTADO_CIVIL
+order by quantidade desc;
